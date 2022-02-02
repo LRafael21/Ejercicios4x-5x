cc7cfaf0c415f0e43ae0e7350504afd48edc02e7
Luis <luis_baltodano@ucne.edu.do>
2022-02-01 23:00:56 -0400

    Ejercicios4-5

---
+++ Ejercicios/Capitulo4Ejercicio1/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _Capitulo4Ejercicio1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+           
+        }
+
+        public void CalcularButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            Validar();
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int Resultado = Convert.ToInt32(NumeroTextBox.Text) * i;
+                MessageBox.Show($"El Resultado de la tabla es {Resultado.ToString()}");
+
+            }
+        }
+
+        private bool Validar()
+        {
+
+            bool esValido = true;
+
+            if (NumeroTextBox.Text.Length <= 0)
+            {
+                esValido = false;
+                MessageBox.Show("Debe Digitar un numero", "Fallo",
+                    MessageBoxButton.OK, MessageBoxImage.Warning);
+            }
+
+            return esValido;
+        }
+
+    }
+}
+
